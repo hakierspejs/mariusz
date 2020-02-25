1a18d77c09078c46a462efe8acaaea652c0191eb
Jacek Wielemborek <d33tah@gmail.com>
2020-02-25 18:06:44 +0100

    Info o tym że bot się włączył.

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def main():
         update_id = None
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.info('Dzialam. Chyba.')
 
     while True:
         try:
