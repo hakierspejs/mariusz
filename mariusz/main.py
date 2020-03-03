c7e965faa24860387626c0599b07bca40b780faa
gizlu <gizlu@mailbox.org>
2020-03-03 15:04:31 +0100

    Translate code to english

--- mariusz/main.py
+++ mariusz/main.py
@@ -15,7 +15,7 @@ import telegram
 
 LOGGER = logging.getLogger()
 
-POLSKI_NA_LACINSKI = {
+POLISH_TO_LATIN = {
     'ą': 'a', 'Ą': 'A',
     'ć': 'c', 'Ć': 'C',
     'ę': 'e', 'Ę': 'E',
@@ -28,42 +28,42 @@ POLSKI_NA_LACINSKI = {
 }
 
 
-def normalizuj_slowo(slowo):
-    ret = ''
-    for litera in slowo:
-        ret += POLSKI_NA_LACINSKI.get(litera, litera)
-    LOGGER.debug('normalizuj_slowo(%r)=%r', slowo, ret)
-    return ret
+def normalize_word(word):
+    output = ''
+    for letter in word:
+        output += POLISH_TO_LATIN.get(letter, letter)
+    LOGGER.debug('normalize_word(%r)=%r', word, output)
+    return output
 
 
-def normalizuj(zbior):
-    ret = set()
-    for slowo in zbior:
-        ret.add(slowo)
-        ret.add(normalizuj_slowo(slowo))
-    LOGGER.debug('normalizuj(%r)=%r', zbior, ret)
-    return ret
+def normalize(sequence): #chuj
+    output = set()
+    for word in sequence:
+        output.add(word)
+        output.add(normalize_word(word))
+    LOGGER.debug('normalize(%r)=%r', sequence, output)
+    return output
 
 
 # TODO: przepisać na sqlite, bo wstyd.
-class BazaChatow:
+class ChatDb: #chuj
 
     def __init__(self, fname):
         self.db = sqlite3.connect(fname)
-        self.zaladuj_scheme()
+        self.load_scheme()
 
-    def zaladuj_scheme(self):
+    def load_scheme(self):
         self.db.execute('CREATE TABLE IF NOT EXISTS chat_ids (chat_id TEXT);')
 
-    def dopisz(self, chat_id):
-        if int(chat_id) in self.listuj():
+    def insert(self, chat_id):
+        if int(chat_id) in self.write_out():
             return
         sql = 'INSERT INTO chat_ids(chat_id) VALUES (?)'
         cur = self.db.cursor()
         cur.execute(sql, (chat_id, ))
         self.db.commit()
 
-    def listuj(self):
+    def write_out(self):
         for row in self.db.execute('SELECT chat_id FROM chat_ids'):
             chat_id = int(row[0])
             if chat_id != -1001361809256:
@@ -72,9 +72,9 @@ class BazaChatow:
 
 class Mariusz:
 
-    def __init__(self, api_key, sciezka_do_bazy_chatow):
+    def __init__(self, api_key, path_to_chat_db):
         self.update_id = None
-        self.reakcje = {}
+        self.reactions = {} #chuj
         self.bot = telegram.Bot(api_key)
 
         try:
@@ -82,40 +82,40 @@ class Mariusz:
         except IndexError:
             self.update_id = None
 
-        if sciezka_do_bazy_chatow:
-            self.baza_chatow = BazaChatow(sciezka_do_bazy_chatow)
-            wersja = self.zbuduj_opis_wersji()
-            msg = f'Bot się wita po restarcie. wersja={wersja}'
-            for chat_id in self.baza_chatow.listuj():
+        if path_to_chat_db:
+            self.chat_db = ChatDb(path_to_chat_db)
+            version = self.build_version_description()
+            msg = f'Bot się wita po restarcie. wersja={version}'
+            for chat_id in self.chat_db.write_out():
                 LOGGER.debug('Witam się z chat_id=%r', chat_id)
                 self.bot.send_message(text=msg, chat_id=chat_id)
         else:
-            self.baza_chatow = None
+            self.chat_db = None
 
         self.on(
-            normalizuj({'Łódź', 'Łodzi', 'łódzkie'}),
+            normalize({'Łódź', 'Łodzi', 'łódzkie'}),
             'https://www.youtube.com/watch?v=IJ2kvZpJ_BU'
         )
-        self.on({'.wersja'}, self.wersja)
+        self.on({'.wersja'}, self.version)
         self.on({'jeszcze jak'}, 'https://www.youtube.com/watch?v=_jX3qsyIlHc')
         self.on({'nie wiem'}, 'https://www.youtube.com/watch?v=QnMqRTu4Rcc')
         self.on({'.panjezus'}, 'https://www.youtube.com/watch?v=aWJ8X3mt8Io')
         self.on({'.help', '.pomoc', '.komendy'}, self.help)
         self.on({'.czy'}, self.czy)
 
-    def on(self, slowa, reakcja):
+    def on(self, text, reaction):
         regex_str = '|'.join([
-            '^' + x if x.startswith('.') else x for x in slowa
+            '^' + x if x.startswith('.') else x for x in text
         ])
         regex = re.compile(regex_str, flags=re.IGNORECASE)
-        if isinstance(reakcja, str):
+        if isinstance(reaction, str):
 
-            def powiedz_cos(update):
-                update.message.reply_text(reakcja)
-            powiedz_cos.__doc__ = f'mówi `{reakcja}`'
-            self.reakcje[regex] = powiedz_cos
+            def say(update):
+                update.message.reply_text(reaction)
+            say.__doc__ = f'mówi `{reaction}`'
+            self.reactions[regex] = say
         else:
-            self.reakcje[regex] = reakcja
+            self.reactions[regex] = reaction
 
     # pozyczone od kolegi: https://github.com/yojo2/BillyMays/
     def czy(self, update):
@@ -149,65 +149,65 @@ class Mariusz:
             response = random.choice(responses_dunno)
         update.message.reply_text(response)
 
-    def zbuduj_opis_wersji(self):
+    def build_version_description(self):
         try:
             with open('/tmp/commit-id') as f:
-                wersja = f.read().strip()
+                version = f.read().strip()
             with open('/tmp/commit-no') as f:
-                numer = f.read().strip()
+                number = f.read().strip()
             with open('/tmp/commit-date') as f:
                 data = f.read().strip()
-        except FileNotFoundError:
-            wersja_b = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-            wersja = wersja_b.decode()
+        except FileNotFoundError: #chuj
+            version_b = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+            version = version_b.decode()
             no_b = subprocess.check_output(['git', 'log', 'HEAD', '--oneline'])
-            numer = len(no_b.split(b'\n'))
+            number = len(no_b.split(b'\n'))
             data = subprocess.check_output([
                 'git', 'show', '-s', '--format=%ci', 'HEAD'
             ]).decode().strip()
-        return f'{wersja[:6]} (#{numer}, {data})'
+        return f'{version[:6]} (#{number}, {data})'
 
-    def wersja(self, update):
+    def version(self, update):
         '''Podaje pierwsze 6 znaków hasha commita wersji.'''
-        update.message.reply_text(self.zbuduj_opis_wersji())
+        update.message.reply_text(self.build_version_description())
 
     def help(self, update):
         '''Wyświetla pomoc'''
         msg = ''
-        for reakcja, funkcja in self.reakcje.items():
-            opis = funkcja.__doc__ or funkcja.__name__
-            msg += f'{reakcja.pattern} => {opis}\n'
+        for reaction, function in self.reactions.items():
+            opis = function.__doc__ or function.__name__
+            msg += f'{reaction.pattern} => {opis}\n'
         update.message.reply_text(msg, parse_mode=telegram.ParseMode.MARKDOWN)
 
     def run(self):
 
         while True:
             try:
-                self.obsluz_wiadomosci()
+                self.parse_message()
             except NetworkError:
                 sleep(1)
             except Unauthorized:
                 # The user has removed or blocked the bot.
                 self.update_id += 1
 
-    def obsluz_wiadomosci(self):
+    def parse_message(self):
         for update in self.bot.get_updates(offset=self.update_id, timeout=10):
             self.update_id = update.update_id + 1
             if update.message is None or update.message.text is None:
                 continue
-            if self.baza_chatow:
-                self.baza_chatow.dopisz(update.message.chat_id)
-            for reakcja, funkcja in self.reakcje.items():
-                if reakcja.match(update.message.text):
-                    funkcja(update)
+            if self.chat_db:
+                self.chat_db.insert(update.message.chat_id)
+            for reaction, funtion in self.reactions.items():
+                if reaction.match(update.message.text):
+                    funtion(update)
 
 
 def main():
     logfmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(format=logfmt, level='DEBUG')
     api_key = os.environ['API_KEY']
-    sciezka_do_bazy_chatow = os.environ.get('SCIEZKA_DO_BAZY_CHATOW')
-    Mariusz(api_key, sciezka_do_bazy_chatow).run()
+    path_to_chat_db = os.environ.get('SCIEZKA_DO_BAZY_CHATOW')
+    Mariusz(api_key, path_to_chat_db).run()
 
 
 if __name__ == '__main__':
