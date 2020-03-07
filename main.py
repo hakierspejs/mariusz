be8baad1357e7f213fc0ea612df36c936bc98176
Jacek Wielemborek <d33tah@gmail.com>
2020-03-07 18:36:54 +0100

    Info o meetupie w tytule + refactor

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ import re
 import random
 import os
 import subprocess
-from time import sleep
+import datetime
+import time
+import dateutil.parser
+
+import lxml.etree
+import lxml.html
+import requests
 
 from telegram.error import NetworkError, Unauthorized
 import telegram
@@ -28,6 +34,25 @@ POLSKI_NA_LACINSKI = {
 }
 
 
+NAZWY_DNI = [
+    'poniedziałek', 'wtorek', 'środa',
+    'czwartek', 'piątek', 'sobota', 'niedziela'
+]
+
+
+NAZWY_MIESIECY = [
+    'stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca',
+    'sierpnia', 'września', 'października', 'listopada', 'grudnia'
+]
+
+
+def opisz_date(data):
+    return (
+        f'{NAZWY_DNI[data.weekday()]}, {data.day} {NAZWY_MIESIECY[data.month]}'
+        f' {data.year} o godz {data.hour}:{str(data.minute).zfill(2)}'
+    )
+
+
 def normalizuj_slowo(slowo):
     ret = ''
     for litera in slowo:
@@ -45,7 +70,50 @@ def normalizuj(zbior):
     return ret
 
 
-# TODO: przepisać na sqlite, bo wstyd.
+def sformuluj_powiadomienie_meetupowe():
+
+    prefix = 'https://www.meetup.com/Hakierspejs-%C5%81od%C5%BA/'
+    s = requests.get(prefix + '/events/rss/').text
+    rss = lxml.etree.fromstring(s.encode())
+
+    timestamps = []
+    for item in rss.xpath('//item'):
+        for description in item.xpath('.//description/text()'):
+            h = lxml.html.fromstring(description.encode())
+            p = h.xpath('//p/text()')
+            timestamps.append(dateutil.parser.parse(p[-3]))
+
+    jutro = datetime.datetime.now() + datetime.timedelta(days=1)
+    daty_spotkan = [t for t in timestamps if t > jutro]
+    if not daty_spotkan:
+        return
+    najblizszy = min(daty_spotkan)
+
+    return (
+        f'Nast. spotkanie: {opisz_date(najblizszy)}. Więcej szczegółów: '
+        + prefix + '/events/calendar/'
+    )
+
+
+def zbuduj_opis_wersji():
+    try:
+        with open('/tmp/commit-id') as f:
+            wersja = f.read().strip()
+        with open('/tmp/commit-no') as f:
+            numer = f.read().strip()
+        with open('/tmp/commit-date') as f:
+            data = f.read().strip()
+    except FileNotFoundError:
+        wersja_b = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+        wersja = wersja_b.decode()
+        no_b = subprocess.check_output(['git', 'log', 'HEAD', '--oneline'])
+        numer = len(no_b.split(b'\n'))
+        data = subprocess.check_output([
+            'git', 'show', '-s', '--format=%ci', 'HEAD'
+        ]).decode().strip()
+    return f'{wersja[:6]} (#{numer}, {data})'
+
+
 class BazaChatow:
 
     def __init__(self, fname):
@@ -64,10 +132,8 @@ class BazaChatow:
         self.db.commit()
 
     def listuj(self):
-        for row in self.db.execute('SELECT chat_id FROM chat_ids'):
-            chat_id = int(row[0])
-            if chat_id != -1001361809256:
-                yield chat_id
+        for row in self.db.execute('SELECT DISTINCT chat_id FROM chat_ids'):
+            yield int(row[0])
 
 
 class Mariusz:
@@ -75,6 +141,7 @@ class Mariusz:
     def __init__(self, api_key, sciezka_do_bazy_chatow):
         self.update_id = None
         self.reakcje = {}
+        self.ostatnie_sprawdzenie_meetupa = 0
         self.bot = telegram.Bot(api_key)
 
         try:
@@ -84,9 +151,11 @@ class Mariusz:
 
         if sciezka_do_bazy_chatow:
             self.baza_chatow = BazaChatow(sciezka_do_bazy_chatow)
-            wersja = self.zbuduj_opis_wersji()
+            wersja = zbuduj_opis_wersji()
             msg = f'Bot się wita po restarcie. wersja={wersja}'
             for chat_id in self.baza_chatow.listuj():
+                if chat_id == -1001361809256:
+                    continue
                 LOGGER.debug('Witam się z chat_id=%r', chat_id)
                 self.bot.send_message(text=msg, chat_id=chat_id)
         else:
@@ -147,27 +216,9 @@ class Mariusz:
             response = random.choice(responses_dunno)
         update.message.reply_text(response)
 
-    def zbuduj_opis_wersji(self):
-        try:
-            with open('/tmp/commit-id') as f:
-                wersja = f.read().strip()
-            with open('/tmp/commit-no') as f:
-                numer = f.read().strip()
-            with open('/tmp/commit-date') as f:
-                data = f.read().strip()
-        except FileNotFoundError:
-            wersja_b = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-            wersja = wersja_b.decode()
-            no_b = subprocess.check_output(['git', 'log', 'HEAD', '--oneline'])
-            numer = len(no_b.split(b'\n'))
-            data = subprocess.check_output([
-                'git', 'show', '-s', '--format=%ci', 'HEAD'
-            ]).decode().strip()
-        return f'{wersja[:6]} (#{numer}, {data})'
-
     def wersja(self, update):
         '''Podaje pierwsze 6 znaków hasha commita wersji.'''
-        update.message.reply_text(self.zbuduj_opis_wersji())
+        update.message.reply_text(zbuduj_opis_wersji())
 
     def help(self, update):
         '''Wyświetla pomoc'''
@@ -177,13 +228,34 @@ class Mariusz:
             msg += f'{reakcja.pattern} => {opis}\n'
         update.message.reply_text(msg, parse_mode=telegram.ParseMode.MARKDOWN)
 
+    def obsluz_meetup(self):
+        if self.ostatnie_sprawdzenie_meetupa + 600 > time.time():
+            return
+        wiadomosc = sformuluj_powiadomienie_meetupowe()
+        if not wiadomosc:
+            return
+        self.ostatnie_sprawdzenie_meetupa = time.time()
+        if self.baza_chatow is None:
+            LOGGER.debug('obsluz_meetup(): self.baza_chatow is None')
+            return
+        for chat_id in self.baza_chatow.listuj():
+            chat = self.bot.get_chat(chat_id=chat_id)
+            if chat.pinned_message and chat.pinned_message.text == wiadomosc:
+                continue
+            msg = self.bot.send_message(text=wiadomosc, chat_id=chat_id)
+            if chat_id < 0:  # czy to chat grupowy?
+                self.bot.pin_chat_message(
+                    message_id=msg.message_id, chat_id=msg.chat_id
+                )
+
     def run(self):
 
         while True:
             try:
+                self.obsluz_meetup()
                 self.obsluz_wiadomosci()
             except NetworkError:
-                sleep(1)
+                time.sleep(1)
             except Unauthorized:
                 # The user has removed or blocked the bot.
                 self.update_id += 1
@@ -200,9 +272,13 @@ class Mariusz:
                     funkcja(update)
 
 
-if __name__ == '__main__':
+def main():
     logfmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(format=logfmt, level='DEBUG')
     api_key = os.environ['API_KEY']
     sciezka_do_bazy_chatow = os.environ.get('SCIEZKA_DO_BAZY_CHATOW')
     Mariusz(api_key, sciezka_do_bazy_chatow).run()
+
+
+if __name__ == '__main__':
+    main()
