f4f1d35beba334cc1828ea62e966723b223703e1
cb4b1fd915 <info@hs-ldz.pl>
2023-12-04 12:41:24 +0100

    Improve gnujdb promts

---
+++ mariusz/gnujdb.py
@@ -0,0 +1,21 @@
+import urllib.parse
+
+
+TRIGGERS = {'jest moze', 'jest może', 'czy jest', 'czy mamy', 'mamy może', 'mamy moze', 'może mamy', 'moze mamy'}
+
+
+def czymamy(message: str) -> str | None:
+    url = None
+    hsl_phrases = ('w spejse', 'w spejsie', 'w hackerspejsie', 'w hs-ie', 'w hs', 'w hsie', )
+    null_phrases = ('jakiś', 'jakis', 'może', 'moze', 'mamy')
+    text = message.lower().split('?')[0].strip()
+    if '?' in message and any(map(lambda x: x in text, hsl_phrases)):
+        replacements = ('jakiś')
+        for item in TRIGGERS:
+            text = text.replace(item, '')
+        for item in hsl_phrases:
+            text = text.replace(item, '')
+        for item in null_phrases:
+            text = text.replace(item, '')
+        url = 'https://g.hs-ldz.pl/search?query=' + urllib.parse.quote(text.strip())
+    return url
--- mariusz/main.py
+++ mariusz/main.py
@@ -21,6 +21,7 @@ import mariusz.meetup
 import mariusz.coronavirus as coronavirus
 import mariusz.wiki
 import mariusz.mumble
+import mariusz.gnujdb
 
 LOGGER = logging.getLogger()
 
@@ -147,7 +148,7 @@ class Mariusz:
         )
         self.on({'\\.wersja'}, self.version)
         self.on({'jeszcze jak'}, 'https://www.youtube.com/watch?v=_jX3qsyIlHc')
-        self.on({'czy mamy', 'mamy może', 'mamy moze', 'może mamy', 'moze mamy'}, self.czymamy)
+        self.on(mariusz.gnujdb.TRIGGERS, self.czymamy)
 #        self.on({'nie wiem'}, 'https://www.youtube.com/watch?v=QnMqRTu4Rcc')
         self.on({'\\.panjezus'}, 'https://www.youtube.com/watch?v=aWJ8X3mt8Io')
         self.on({'\\.corobic'}, 'https://www.youtube.com/watch?v=6NR-Lq-hhSw')
@@ -234,13 +235,9 @@ class Mariusz:
         update.message.reply_text(build_version_description())
 
     def czymamy(self, update):
-        text = update.message.text.lower().strip()
-        if text.endswith('?'):
-            replacements = ('czy mamy', 'mamy może', 'mamy moze', 'może mamy', 'moze mamy', 'jakiś', 'w hsie', 'w spejsie', 'w hackerspejsie', 'w hs-ie')
-            for item in replacements:
-                text = text.replace(item, '')
-            q = text.replace('?', '').strip()
-            url = 'https://g.hs-ldz.pl/search?query=' + urllib.parse.quote(q)
+        # Czy jest w spejse pickit 2?
+        url = mariusz.gnujdb.czymamy(update.message.text)
+        if url:
             update.message.reply_text(url)
 
     def help(self, update):
